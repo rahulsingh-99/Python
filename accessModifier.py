43599c20be10c36d7ff2b14a02d703fe8a2aec8b
rahul <rahul.rock.rr99@gmail.com>
2023-07-08 01:19:04 +0530

    commit

--- accessModifier.py
+++ accessModifier.py
@@ -5,10 +5,11 @@ class Person:
         self._age = age     #! protected
         self.address = address #!public
     
-    def __displayName(self):
+    def displayName(self):
         print("Name =", self.__name)
 
 P = Person("Rahul", 22, "Delhi")
 print("Address =",P.address)
 print("Age =",P._age)
-P._Person__displayName()
+P.displayName()
+#P._Person__displayName() #! this is for displaying private method but not recommend anyone for this
